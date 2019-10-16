8c7b5b5be1f4aeb29d64bb45efa56f6fadea1522
russellwinters <russellwinters@protonmail.com>
2019-10-16 12:00:46 -0700

    Made the table only above 600px breakpoint. Also styled a little bit

--- sprint-two/scripts/table.js
+++ sprint-two/scripts/table.js
@@ -1,49 +1,45 @@
 // !Object that contains all show information
-var shows = [
+const shows = [
   {
-    Dates: "Mon Dec 17 2018",
-    Venue: "Ronald Lane",
-    Location: "San Fransisco, CA"
+    dates: "Mon Dec 17 2018",
+    venue: "Ronald Lane",
+    location: "San Fransisco, CA"
   },
   {
-    Dates: "Tue Jul 18 2019",
-    Venue: "Pier 3 East",
-    Location: "San Fransisco, CA"
+    dates: "Tue Jul 18 2019",
+    venue: "Pier 3 East",
+    location: "San Fransisco, CA"
   },
   {
-    Dates: "Fri Jul 22 2019",
-    Venue: "View Lounge",
-    Location: "San Fransisco, CA"
+    dates: "Fri Jul 22 2019",
+    venue: "View Lounge",
+    location: "San Fransisco, CA"
   },
   {
-    Dates: "Wed Aug 11 2019",
-    Venue: "Pres Club",
-    Location: "San Fransisco, CA"
+    dates: "Wed Aug 11 2019",
+    venue: "Pres Club",
+    location: "San Fransisco, CA"
   },
   {
-    Dates: "Sat Aug 12 2019",
-    Venue: "Hyatt Agency",
-    Location: "San Fransisco, CA"
+    dates: "Sat Aug 12 2019",
+    venue: "Hyatt Agency",
+    location: "San Fransisco, CA"
   },
   {
-    Dates: "Fri Sep 05 2019",
-    Venue: "Moscow Center",
-    Location: "San Fransisco, CA"
+    dates: "Fri Sep 05 2019",
+    venue: "Moscow Center",
+    location: "San Fransisco, CA"
   }
 ];
-
-// !Create variables that will be used in both functions
-var tableRow;
-var createText;
 
 // !Create a function for the table headers
 
 function createTableHeading(thisTable, keyValues) {
-  var tHead = thisTable.createTHead();
-  tableRow = tHead.insertRow();
-  for (var show of showKeys) {
-    var th = document.createElement("th");
-    createText = document.createTextNode(show);
+  let tHead = thisTable.createTHead();
+  let tableRow = tHead.insertRow();
+  for (showKey of showKeys) {
+    let th = document.createElement("th");
+    let createText = document.createTextNode(showKey);
     th.appendChild(createText);
     tableRow.appendChild(th);
   }
@@ -52,24 +48,27 @@ function createTableHeading(thisTable, keyValues) {
 // !Function to create table data
 function createTable(thisTable, data) {
   for (var show of shows) {
-    tableRow = thisTable.insertRow();
+    let tableRow = thisTable.insertRow();
     for (var key in show) {
-      var cell = tableRow.insertCell();
-      createText = document.createTextNode(show[key]);
+      let cell = tableRow.insertCell();
+      let createText = document.createTextNode(show[key]);
       cell.appendChild(createText);
     }
-    var btnCell = tableRow.insertCell();
+    let btnCell = tableRow.insertCell();
     btnCell.className = "button-container";
-    var btn = document.createElement("a");
+    let btn = document.createElement("a");
     btn.innerHTML = "Buy tickets";
     btnCell.appendChild(btn);
   }
 }
 
 // !Invoking variables
-var myTable = document.querySelector("table");
-var showKeys = Object.keys(shows[0]);
+const myTable = document.querySelector("table");
+const showKeys = Object.keys(shows[0]);
+const tabletWidth = window.innerWidth;
 
 // !Invoke functions
-createTableHeading(myTable, showKeys);
-createTable(myTable, shows);
+if (tabletWidth >= 600) {
+  createTable(myTable, shows);
+  createTableHeading(myTable, showKeys);
+}
