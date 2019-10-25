e4eb6ec721cb6b86c2599d6ff98c6830adb59c2b
russ_winters <russellwinters@protonmail.com>
2019-10-25 11:50:58 -0700

    Fixed bug with comment section deleting oldest comment

--- sprint-three/scripts/comments.js
+++ sprint-three/scripts/comments.js
@@ -3,7 +3,7 @@ const projectKey = axios
   .then(resp => console.log(resp));
 const apiKey = "9a717c8f-9d92-47f2-a204-ee2b162ac82a";
 
-// !Date Function: Unlike the button function on the table I made this up here so I could just invoke it below.
+// Date Function: Unlike the button function on the table I made this up here so I could just invoke it below.
 const dateFunction = () => {
   //this needs to change so it runs of epoch time.
   //To do so you need a variable for the epoch time, then run that variable through a new Date(variable) to output something that "today" can run.
@@ -23,41 +23,41 @@ function epochTimeFunction(num) {
 const today = dateFunction();
 // console.log(today); //Only used this to check that today function was working
 
-// !Array that will help to replace OG comment section
+// Array that will help to replace OG comment section
 var tags = [];
 
-// !Function that will render the first three objects inside our array at the top.
+// Function that will render the first three objects inside our array at the top.
 function renderComments(thisSection, arr) {
   for (i = 0; i < arr.length; i++) {
     // if this breaks it was i < 3
     let objNumber = i;
 
-    //! create div within section
+    // create div within section
     let commentContainer = document.createElement("div");
     commentContainer.className = "comment-div";
     thisSection.appendChild(commentContainer);
 
-    //! create div inside previous div with classname of .image-container that can be styled in SCSS
+    // create div inside previous div with classname of .image-container that can be styled in SCSS
     let imageContainer = document.createElement("div");
     imageContainer.className = "image-container";
     commentContainer.appendChild(imageContainer);
 
-    //! create image within this container - I repeated this image in all comments because it's the only one we have access to.
+    // create image within this container - I repeated this image in all comments because it's the only one we have access to.
     let newImg = document.createElement("img");
     newImg.setAttribute("src", "./assets/images/Mohan-muruge.jpg");
     imageContainer.appendChild(newImg);
 
-    //! create another div for content with classname .form-content
+    // create another div for content with classname .form-content
     let formContent = document.createElement("div");
     formContent.className = "form-content";
     commentContainer.appendChild(formContent);
 
-    //! create div inside previous div for name and timestamp
+    // create div inside previous div for name and timestamp
     let postId = document.createElement("div");
     postId.className = "post-id";
     formContent.appendChild(postId);
 
-    //! Creating spans for both the name and time part of the input.
+    // Creating spans for both the name and time part of the input.
     let nameStamp = document.createElement("span");
     nameStamp.className = "name-stamp";
     postId.appendChild(nameStamp);
@@ -65,12 +65,12 @@ function renderComments(thisSection, arr) {
     timeStamp.className = "time-stamp";
     postId.appendChild(timeStamp);
 
-    //! creating div for comment, just like the above inputs
+    // creating div for comment, just like the above inputs
     let commentStatus = document.createElement("div");
     commentStatus.className = "comment-output";
     formContent.appendChild(commentStatus);
 
-    //! creating text node for each object value;
+    // creating text node for each object value;
     let commentOutput = document.createTextNode(arr[objNumber]["comment"]);
     let nameOutput = document.createTextNode(arr[objNumber]["name"]);
     let timeOutput = document.createTextNode(
@@ -80,7 +80,7 @@ function renderComments(thisSection, arr) {
     timeStamp.appendChild(timeOutput);
     commentStatus.appendChild(commentOutput);
 
-    // !Creating a new object that contains references for each of the text nodes - so that I can replace their content when form is submitted.
+    // Creating a new object that contains references for each of the text nodes - so that I can replace their content when form is submitted.
     tags[i] = {
       name: nameOutput,
       comment: commentOutput,
@@ -89,10 +89,10 @@ function renderComments(thisSection, arr) {
   }
 }
 
-// !QuerySelector for the seciton so that I can then invoke the function to render comments on following line
+// QuerySelector for the seciton so that I can then invoke the function to render comments on following line
 const thisSection = document.querySelector(".comment-display");
 
-//! Render comments from API array
+// Render comments from API array
 var theGivenComments = [];
 const theCommentSection = axios
   .get("https://project-1-api.herokuapp.com/comments?api_key=" + apiKey)
@@ -100,23 +100,30 @@ const theCommentSection = axios
     theGivenComments = response.data;
     renderComments(thisSection, theGivenComments);
   });
-
-// //! Purpose of this function: at the event of submission, update array with submitted text and re-render that comment section
+const newSection = document.createElement("section"); // this section is going to replace the current section so that I can re-render the entire section upon comment suubmission.
+//Create function to delete elements
+function deleteComments() {
+  // let section = document.querySelector(".comment-display");
+  // newSection = document.createElement("section");
+  newSection.classList.add("comment-display");
+  thisSection.replaceWith(newSection);
+}
+// Purpose of this function: at the event of submission, update array with submitted text and re-render that comment section
 var form = document.querySelector("#comments");
 form.addEventListener("submit", click => {
   click.preventDefault();
-  //! create variables for submitted content
+  // create variables for submitted content
   let commentName = event.target.name.value;
   let commentStatus = event.target.comment.value;
   let newTimeStamp = new Date().getTime(); //Need to request the date in epoch time
-  //! create object containing each variable with content
+  // create object containing each variable with content
   let newComment = {
     name: commentName,
     comment: commentStatus,
     timestamp: newTimeStamp
   };
   tags.push(newComment);
-  //!Following lines to create object to sent to API server and then to actually send it.
+  //Following lines to create object to sent to API server and then to actually send it.
   let axiosComment = {
     name: commentName,
     comment: commentStatus
@@ -129,10 +136,11 @@ form.addEventListener("submit", click => {
     )
     .then(response => console.log(response.data));
 
-  //! unshift() object into array
+  // unshift() object into array
   theGivenComments.unshift(newComment); //This is only necesary so that it now updates the comment section.
-  //* renderComments(thisSection, theGivenComments); //this will work if I can delete last renderComments(section, arr) iteration
-  //! run for loop to update comment section by updating the nodeValue from the particular nodes that are being referenced from tags[].
+  deleteComments();
+  renderComments(newSection, theGivenComments); //this will work if I can delete last renderComments(section, arr) iteration
+  // run for loop to update comment section by updating the nodeValue from the particular nodes that are being referenced from tags[].
   for (i = 0; i < tags.length; i++) {
     //if this breaks it's same issue as above
     tags[i].name.nodeValue = theGivenComments[i]["name"];
@@ -142,11 +150,10 @@ form.addEventListener("submit", click => {
     ); //This is me trying to output date from epoch time. Success!!
   }
 
-  //! Reset the form so it's fresh after submission.
+  // Reset the form so it's fresh after submission.
   form.reset();
 });
 
-//*Having an issue where it isn't wanting to render another comment. do I have to create another in this section? Could I delete the old comment section and recall renderComments()?
 //Thinking to make the comments render in the right order, while also having the new comments refresh in the correct order have line 133 act as:
 //     - theGivenComments.push(newComment)
 //     - Have renderComments() for loop act as i = arr.length - 1; i >= 0; i--
